558426c3cf13c181910adbeaf751e1d325aca5d5
nakamura <hrm.nkmr04@gmail.com>
2024-08-02 08:03:38 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function alertFunction1() {
+	alert("文字列はダブルクォーテーションかシングルクォーテーションで囲みます");
+}
+alertFunction1()
+const getRectArea = function(width, height) {
+	return width * height;
+};
+console.log(getRectArea(3, 4));
